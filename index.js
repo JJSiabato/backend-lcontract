6f790d69f0a29a77bd36174827133ad6f675faec
John <jherzon.siabato@gmail.com>
2022-04-20 15:45:03 -0500

    add heroku config

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use((request, response)=>{
     
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
     console.log(`El servidor corre en el puerto: ${PORT}`)
 })
